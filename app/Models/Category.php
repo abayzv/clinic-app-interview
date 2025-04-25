3ba953b5f1a3e7b7f4dc714c7b05f309d9a7307e
abayzv <ajipunk008@gmail.com>
2025-04-25 20:06:49 +0700

    init category crud

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+    ];
+
+    public function scopeWithNewOrders($query)
+    {
+        return $query->with(['orders' => function ($q) {
+            $q->where('created_at', '>', Carbon::today()->subWeek());
+        }]);
+    }
 }
