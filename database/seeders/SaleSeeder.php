2361954b235a4b6af36a4380228612dd210e0ca4
abayzv <ajipunk008@gmail.com>
2025-04-26 08:53:26 +0700

    (fix) add customer id to sale model

--- database/seeders/SaleSeeder.php
+++ database/seeders/SaleSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Customer;
 use App\Models\Sale;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,10 +14,12 @@ class SaleSeeder extends Seeder
      */
     public function run(): void
     {
+        $customers = Customer::pluck('id')->toArray();
+
         for ($i = 1; $i <= 10; $i++) {
             Sale::create([
                 'invoice_number' => 'INV' . str_pad($i, 5, '0', STR_PAD_LEFT),
-                'customer_name' => fake()->name(),
+                'customer_id' => fake()->randomElement($customers),
                 'discount' => rand(0, 10000),
                 'total_price' => 0,
             ]);
